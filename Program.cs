41d79b9a281e215adaa568be464a32830233b0d2
Oleg <olmag@inbox.ru>
2023-01-14 20:21:52 +0300

    commit2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя: ");
+string username = console.ReadLine();
+
+if (username == "Маша")
+{
+
+}
